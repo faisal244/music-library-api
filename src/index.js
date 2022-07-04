bad387d523905461e1e52d57b01e7071f5492415
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-07-04 16:19:11 +0100

    Creates files and folder structure

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const PORT = process.env.PORT || 4000;
 // Declare your middlewares here
 // JSON is a method
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
+// Do this next bit at the end, because your app wills tart to break if not. Do your routes folders and files first
 app.listen(PORT, () => {
 	console.log(`listening to port on http://locaalhost:${PORT}`);
 });
